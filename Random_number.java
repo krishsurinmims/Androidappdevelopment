4d68be118775c90606e6f0e21018a7623c1d7bf7
krishsurinmims <96473464+krishsurinmims@users.noreply.github.com>
2022-01-03 09:31:33 +0530

    Create Random_number.java

---
+++ Random_number.java
@@ -0,0 +1,24 @@
+package com.assignments;
+import java.lang.Math;
+
+public class RandomNumber {
+    public static void main(String[] args) {
+        //Selects a random  number and multiplies to avoid decimal 0. .....
+
+        int rand = (int) (Math.random()*1000);
+        System.out.println("Your Random is " + rand);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
